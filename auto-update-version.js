f1ffb6b9191fd9b0c93daf2f272749a9cedb74c9
ericchentch <ericchen.tch@gmail.com>
2023-09-22 17:59:18 +0700

    add auto change version

---
+++ auto-update-version.js
@@ -0,0 +1,51 @@
+const https = require("https");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const getPackageLatestVersion = async () => {
+  return new Promise((resolve, reject) => {
+    https
+      .get(`https://registry.npmjs.org/@colop/common-factory-next`, (resp) => {
+        let data = "";
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+        resp.on("end", () => {
+          try {
+            resolve(JSON.parse(data)["dist-tags"]["latest"]);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+};
+
+const updatePackageJson = () => {
+  //read package.json
+
+  getPackageLatestVersion().then((v) => {
+    try {
+      const packageJson = JSON.parse(
+        fs.readFileSync("./package.json", { encoding: "utf8" })
+      );
+      const currentVer = v;
+      const currentVerNum = Number(currentVer.split(".").join(""));
+      const newVerNum = currentVerNum + 1;
+      const newVer = String(newVerNum).split("").join(".");
+      //write package.json if needed
+      fs.writeFileSync(
+        "./package.json",
+        JSON.stringify({ ...packageJson, version: newVer })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+updatePackageJson();
